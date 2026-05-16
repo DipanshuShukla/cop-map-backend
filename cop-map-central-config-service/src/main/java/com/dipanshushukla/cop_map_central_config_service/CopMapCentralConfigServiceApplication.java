a0a3fd7f60916fda04c12d7cd2068eb4ed616906
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-17 05:21:27 +0530

    Add configuration files for Cop Map services and enable config server

--- cop-map-central-config-service/src/main/java/com/dipanshushukla/cop_map_central_config_service/CopMapCentralConfigServiceApplication.java
+++ cop-map-central-config-service/src/main/java/com/dipanshushukla/cop_map_central_config_service/CopMapCentralConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.dipanshushukla.cop_map_central_config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CopMapCentralConfigServiceApplication {
 
 	public static void main(String[] args) {
