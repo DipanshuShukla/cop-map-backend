d07e20f9baa9e4d9fb92140e288e93c453e63737
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-17 05:22:36 +0530

    feat: Add Eureka server support and replace application.properties with application.yml

--- cop-map-central-registry-service/src/main/java/com/dipanshushukla/cop_map_central_registry_service/CopMapCentralRegistryServiceApplication.java
+++ cop-map-central-registry-service/src/main/java/com/dipanshushukla/cop_map_central_registry_service/CopMapCentralRegistryServiceApplication.java
@@ -2,8 +2,10 @@ package com.dipanshushukla.cop_map_central_registry_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CopMapCentralRegistryServiceApplication {
 
 	public static void main(String[] args) {
