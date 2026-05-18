04f278e2b462624c1cf3cb0dead2b908e8d9c6d2
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-19 00:24:32 +0530

    init

---
+++ cop-map-location-service/src/main/java/com/dipanshushukla/cop_map_location_service/CopMapLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_location_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopMapLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopMapLocationServiceApplication.class, args);
+	}
+
+}
