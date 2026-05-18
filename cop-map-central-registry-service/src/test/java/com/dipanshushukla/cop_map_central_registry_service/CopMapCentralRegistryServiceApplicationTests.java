408e7e1b376d7149fbcb55b2a338edc270382b2a
Dipanshu Shukla <shuklascientist5@gmail.com>
2026-05-19 01:58:12 +0530

    Imported cop-map-central-registry-service main branch cleanly

---
+++ cop-map-central-registry-service/src/test/java/com/dipanshushukla/cop_map_central_registry_service/CopMapCentralRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.cop_map_central_registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopMapCentralRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
